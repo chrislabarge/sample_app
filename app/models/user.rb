859f8827fe8595a6fcce9acdc27c4546a6b54489
chrislabarge <chrislabarge2@gmail.com>
2015-04-29 18:45:11 -0400

    finished chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 		
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -52,6 +52,22 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end
 
+  #Creates reset_digest
+  def create_reset_digest
+    self.reset_token = User.new_token
+    self.update_attribute(:reset_digest, User.digest(reset_token))
+    self.update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password_reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired
+  def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+  end
 
   private
       #Converts email to all lower-case
@@ -65,5 +81,8 @@ class User < ActiveRecord::Base
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+    
+    
 
 end
